b6acab6c39a3c5a9ae4aad1572a921c5974aae5c
Santhosh-VA <santhosh.venkatachalam@thegravity.ai>
2026-05-08 17:11:13 +0530

    debug mysql

--- conn.php
+++ conn.php
@@ -1,25 +1,16 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn = @mysqli_connect(
+    getenv("DB_HOST"),
+    getenv("DB_USER"),
+    getenv("DB_PASS"),
+    getenv("DB_NAME"),
+    getenv("DB_PORT")
+);
 
-try {
-
-    $conn = mysqli_connect(
-        getenv("DB_HOST"),
-        getenv("DB_USER"),
-        getenv("DB_PASS"),
-        getenv("DB_NAME"),
-        getenv("DB_PORT")
-    );
-
-    echo "DB CONNECTED";
-
-} catch (Exception $e) {
-
-    die("MYSQL ERROR: " . $e->getMessage());
+if (!$conn) {
+    echo mysqli_connect_error();
+    exit;
 }
 
 ?>
